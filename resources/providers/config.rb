688f0772ee01bd144e6d114b930cd9f02f485597
vimesa <vimesa@redborder.com>
2022-01-19 11:46:49 +0000

    Regsiter to consul 1.11.1

--- resources/providers/config.rb
+++ resources/providers/config.rb
@@ -223,7 +223,7 @@ action :register do #Usually used to register in consul
       json_query = Chef::JSONCompat.to_json(query)
 
       execute 'Register service in consul' do
-        command "curl http://localhost:8500/v1/agent/service/register -d '#{json_query}' &>/dev/null"
+        command "curl -X PUT http://localhost:8500/v1/agent/service/register -d '#{json_query}' &>/dev/null"
         action :nothing
       end.run_action(:run)
 
@@ -239,7 +239,7 @@ action :deregister do #Usually used to deregister from consul
   begin
     if node["freeradius"]["registered"]
       execute 'Deregister service in consul' do
-        command "curl http://localhost:8500/v1/agent/service/deregister/freeradius-#{node["hostname"]} &>/dev/null"
+        command "curl -X PUT http://localhost:8500/v1/agent/service/deregister/freeradius-#{node["hostname"]} &>/dev/null"
         action :nothing
       end.run_action(:run)
 

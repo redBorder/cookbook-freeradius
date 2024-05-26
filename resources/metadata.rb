cf14abea0d3dda839760a0d368b14de828fb587e
Miguel Negrón <45871721+manegron@users.noreply.github.com>
2024-05-26 09:55:36 +0100

    Update metadata.rb

--- resources/metadata.rb
+++ resources/metadata.rb
@@ -1,7 +1,6 @@
 name             'freeradius'
-maintainer       'redborder'
-maintainer_email 'vimesa@redborder.com'
-license          'All rights reserved'
+maintainer       'Eneo Tecnología S.L.'
+maintainer_email 'git@redborder.com'
+license          'AGPL-3.0'
 description      'Installs/Configures cookbook-freeradius'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.0.4'
